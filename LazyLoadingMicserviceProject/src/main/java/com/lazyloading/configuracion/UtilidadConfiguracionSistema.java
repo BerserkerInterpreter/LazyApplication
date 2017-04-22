f90b3ad303f463972c89912830951fde4587e9d6
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-21 23:11:15 -0500

    Clase de configuracion del sistema
    Esta clase se encarga de obtener las variables de configuración principales del sistema.

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/configuracion/UtilidadConfiguracionSistema.java
@@ -0,0 +1,58 @@
+package com.lazyloading.configuracion;
+
+import java.util.ResourceBundle;
+
+public final class UtilidadConfiguracionSistema {
+	
+	private static final String RES_BUNDLE_CONFIGURACION = "com.lazyloading.configuracion.configuracion";
+	private static final String LENGUAJE = "com.lazyloading.configuracion.locale.lenguaje";
+	private static final String PAIS = "com.lazyloading.configuracion.locale.pais";
+	private static final String RES_BUNDLE_MENSAJES_BASE_NAME = "com.lazyloading.configuracion.locale.resbundlemensajesbasename";
+	
+	private static final String SEPARADOR = "com.lazyloading.configuracion.tokenizacion.separador";
+	private static final String REEMPLAZO = "com.lazyloading.configuracion.tokenizacion.reemplazo";
+	private static final String UNIDAD_PESO = "com.lazyloading.configuracion.peso.unidadpeso";
+	
+	public static String getResBundleMensajesBaseName() {
+		ResourceBundle resourceBundle = 
+				ResourceBundle.getBundle(RES_BUNDLE_CONFIGURACION);
+		String mensajesBaseName = resourceBundle.getString(RES_BUNDLE_MENSAJES_BASE_NAME);
+		return mensajesBaseName;
+	}
+	
+	public static String getLenguaje() {
+		ResourceBundle resourceBundle = 
+				ResourceBundle.getBundle(RES_BUNDLE_CONFIGURACION);
+		String lenguaje = resourceBundle.getString(LENGUAJE);
+		return lenguaje;
+	}
+	
+	public static String getPais() {
+		ResourceBundle resourceBundle = 
+				ResourceBundle.getBundle(RES_BUNDLE_CONFIGURACION);
+		String pais = resourceBundle.getString(PAIS);
+		return pais;
+	}
+	
+	public static String getSeparador() {
+		ResourceBundle resourceBundle = 
+				ResourceBundle.getBundle(RES_BUNDLE_CONFIGURACION);
+		String separador = resourceBundle.getString(SEPARADOR);
+		return separador;
+	}
+	
+	public static String getReemplazo() {
+		ResourceBundle resourceBundle = 
+				ResourceBundle.getBundle(RES_BUNDLE_CONFIGURACION);
+		String reemplazo = resourceBundle.getString(REEMPLAZO);
+		return reemplazo;
+	}
+	
+	public static String getUnidadPeso() {
+		ResourceBundle resourceBundle = 
+				ResourceBundle.getBundle(RES_BUNDLE_CONFIGURACION);
+		String unidadPeso = resourceBundle.getString(UNIDAD_PESO);
+		return unidadPeso;
+	}
+	
+}
