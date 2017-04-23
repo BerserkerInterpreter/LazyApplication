fb0619f7b5cf336c1cca54b7a836038a9c30dc9e
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 18:53:54 -0500

    Creacion de la clase ExcepcionVerificarVecesDos
    Se adiciona el archivo de creacion de la clase ExcepcionVerificarVecesDos.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/excepciones/ExcepcionVerificarVecesDos.java
@@ -0,0 +1,26 @@
+package com.lazyloading.excepciones;
+
+public class ExcepcionVerificarVecesDos extends ExcepcionDiasPeriodo {
+
+	public ExcepcionVerificarVecesDos() {
+		super();
+	}
+
+	public ExcepcionVerificarVecesDos(String message) {
+		super(message);
+	}
+
+	public ExcepcionVerificarVecesDos(Throwable cause) {
+		super(cause);
+	}
+
+	public ExcepcionVerificarVecesDos(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExcepcionVerificarVecesDos(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
