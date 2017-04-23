902b5c8d4f68f832777d44c4d010c1245682120e
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 18:40:44 -0500

    Creacion de la clase ExcepcionVerificarAtrasListaElementos
    Se adiciona el archivo de creacion de la clase ExcepcionVerificarAtrasListaElementos.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/excepciones/ExcepcionVerificarAtrasListaElementos.java
@@ -0,0 +1,26 @@
+package com.lazyloading.excepciones;
+
+public class ExcepcionVerificarAtrasListaElementos extends ExcepcionDias {
+
+	public ExcepcionVerificarAtrasListaElementos() {
+		super();
+	}
+
+	public ExcepcionVerificarAtrasListaElementos(String message) {
+		super(message);
+	}
+
+	public ExcepcionVerificarAtrasListaElementos(Throwable cause) {
+		super(cause);
+	}
+
+	public ExcepcionVerificarAtrasListaElementos(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExcepcionVerificarAtrasListaElementos(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
