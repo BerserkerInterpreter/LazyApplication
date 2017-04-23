c8d7f466471fc015d64f86b01f7aa3060e395771
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 18:38:08 -0500

    Creacion de la clase ExcepcionObtenerInteger.java
    Se adiciona el archivo de creacion de la clase ExcepcionObtenerInteger.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/excepciones/ExcepcionObtenerInteger.java
@@ -0,0 +1,26 @@
+package com.lazyloading.excepciones;
+
+public class ExcepcionObtenerInteger extends ExcepcionDiasPeriodo {
+
+	public ExcepcionObtenerInteger() {
+		super();
+	}
+
+	public ExcepcionObtenerInteger(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ExcepcionObtenerInteger(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExcepcionObtenerInteger(String message) {
+		super(message);
+	}
+
+	public ExcepcionObtenerInteger(Throwable cause) {
+		super(cause);
+	}
+
+}
