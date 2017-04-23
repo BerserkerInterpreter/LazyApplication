85e67b3ec05b96d70d2d47483dd9cfd32cf4a9ff
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 18:47:38 -0500

    Crecion de la clase ExcepcionVerificarContadorArrDiasElementos
    Se adiciona el archivo de creacion de la clase ExcepcionVerificarContadorArrDiasElementos.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/excepciones/ExcepcionVerificarContadorArrDiasElementos.java
@@ -0,0 +1,26 @@
+package com.lazyloading.excepciones;
+
+public class ExcepcionVerificarContadorArrDiasElementos extends ExcepcionDiasPeriodo {
+
+	public ExcepcionVerificarContadorArrDiasElementos() {
+		super();
+	}
+
+	public ExcepcionVerificarContadorArrDiasElementos(String message) {
+		super(message);
+	}
+
+	public ExcepcionVerificarContadorArrDiasElementos(Throwable cause) {
+		super(cause);
+	}
+
+	public ExcepcionVerificarContadorArrDiasElementos(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExcepcionVerificarContadorArrDiasElementos(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
