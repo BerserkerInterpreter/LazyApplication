fbb4db56afb1ebacd22786548950e02453928ba6
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 18:51:55 -0500

    Creacion de la clase ExcepcionVerificarDiasElementos
    Se adiciona el archivo de creacion de la clase ExcepcionVerificarDiasElementos.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/excepciones/ExcepcionVerificarDiasElementos.java
@@ -0,0 +1,26 @@
+package com.lazyloading.excepciones;
+
+public class ExcepcionVerificarDiasElementos extends ExcepcionDiasPeriodo {
+
+	public ExcepcionVerificarDiasElementos() {
+		super();
+	}
+
+	public ExcepcionVerificarDiasElementos(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ExcepcionVerificarDiasElementos(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExcepcionVerificarDiasElementos(String message) {
+		super(message);
+	}
+
+	public ExcepcionVerificarDiasElementos(Throwable cause) {
+		super(cause);
+	}
+
+}
