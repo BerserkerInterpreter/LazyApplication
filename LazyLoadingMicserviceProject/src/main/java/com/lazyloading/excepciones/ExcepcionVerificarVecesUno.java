f07c2fc714eb65de2fd0a1cd7a3acf775333dab1
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 18:54:36 -0500

    Creacion de la clase ExcepcionVerificarVecesUno
    Se adiciona el archivo de creacion de la clase ExcepcionVerificarVecesUno.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/excepciones/ExcepcionVerificarVecesUno.java
@@ -0,0 +1,26 @@
+package com.lazyloading.excepciones;
+
+public class ExcepcionVerificarVecesUno extends ExcepcionDiasPeriodo {
+
+	public ExcepcionVerificarVecesUno() {
+		super();
+	}
+
+	public ExcepcionVerificarVecesUno(String message) {
+		super(message);
+	}
+
+	public ExcepcionVerificarVecesUno(Throwable cause) {
+		super(cause);
+	}
+
+	public ExcepcionVerificarVecesUno(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExcepcionVerificarVecesUno(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
