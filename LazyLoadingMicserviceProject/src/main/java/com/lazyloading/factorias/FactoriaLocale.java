d8513ecc665ccde093904aa762ac090843d0a1af
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 19:01:26 -0500

    Creacion de la clase FactoriaLocale
    Se adiciona el archivo de creacion de la clase ExcepcionDias.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/factorias/FactoriaLocale.java
@@ -0,0 +1,36 @@
+package com.lazyloading.factorias;
+
+import java.util.Locale;
+
+import com.jcabi.aspects.Loggable;
+import com.lazyloading.configuracion.UtilidadConfiguracionSistema;
+
+public class FactoriaLocale {
+
+	private static FactoriaLocale factoriaLocale;
+	
+	private Locale locale;
+	
+	@Loggable
+	public Locale getLocale() {
+		
+		String lenguaje = UtilidadConfiguracionSistema.getLenguaje();
+		String pais = UtilidadConfiguracionSistema.getPais();
+		
+		if(locale == null) {
+			locale = new Locale.Builder().setLanguage(lenguaje).setRegion(pais).build();
+		}
+		
+		return locale;
+	}
+	
+	public static FactoriaLocale getFactoriaLocale() {
+		
+		if(factoriaLocale == null){
+			factoriaLocale = new FactoriaLocale();
+		}
+		
+		return factoriaLocale;
+	}
+	
+}
