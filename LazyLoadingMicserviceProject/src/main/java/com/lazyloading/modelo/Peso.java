2da268ca470f7322e3b16e6756c57c651d5eb473
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 19:16:19 -0500

    Creacion de la clase Peso
    Se adiciona el archivo de creacion de la clase Peso.class

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/modelo/Peso.java
@@ -0,0 +1,41 @@
+package com.lazyloading.modelo;
+
+public class Peso implements Comparable<Peso> {
+
+	private int valor;
+	private UnidadPeso unidadPeso;
+	
+	public Peso() {}
+	
+	public Peso(int valor, UnidadPeso unidadPeso) {
+		super();
+		this.valor = valor;
+		this.unidadPeso = unidadPeso;
+	}
+	
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	public UnidadPeso getUnidadPeso() {
+		return unidadPeso;
+	}
+	public void setUnidadPeso(UnidadPeso unidadPeso) {
+		this.unidadPeso = unidadPeso;
+	}
+
+	@Override
+	public int compareTo(Peso otroPeso) {
+		
+		if(this.getValor() == otroPeso.getValor()) {
+			return 0;
+		} else if(this.getValor() > otroPeso.getValor()) {
+			return 1;
+		} else {
+			return -1;
+		}
+		
+	}
+}
