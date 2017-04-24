8bb6e1798d11cab8d7009a1f3b21783b9fa3decf
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-04-23 19:43:17 -0500

    Creacion de la clase LazyLoadingMicserviceProjectApplicationTests
    Se adiciona el archivo de creacion de la clase LazyLoadingMicserviceProjectApplicationTests.class

---
+++ LazyLoadingMicserviceProject/src/test/java/com/lazyloading/LazyLoadingMicserviceProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lazyloading;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LazyLoadingMicserviceProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
