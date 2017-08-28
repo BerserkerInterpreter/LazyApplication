521e33c34b6f57bcaa97d87a35d49727169c753c
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-08-28 16:50:36 -0500

    FactoriaControladores class is droped
    There's no need for a controllers factory class because is better a singleton to getting an state.

--- LazyLoadingMicserviceProject/src/main/java/com/lazyloading/fachada/FachadaLazyLoading.java
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/fachada/FachadaLazyLoading.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lazyloading.controladores.ControladorNumMaxViajes;
-import com.lazyloading.factorias.FactoriaControladores;
+import com.lazyloading.controlador.ControladorNumMaxViajes;
 
 @RestController
 @RequestMapping("/api")
@@ -17,21 +16,13 @@ public class FachadaLazyLoading {
 	@RequestMapping(method = RequestMethod.GET, value = "/obtenerMaximoNumeroViajes",
 			produces = "application/json")
 	public ResponseEntity<String> obtenerMaximoNumeroViajes(String diasElementos) {
-		
-		FactoriaControladores factoriaControladores =
-				FactoriaControladores.getFactoriaControladores();
 		ControladorNumMaxViajes controladorNumMaxViajes =
-				factoriaControladores.getControladorNumMaxViajes();
-		
+				ControladorNumMaxViajes.getControladorNumMaxViajes();
 		String listaLineaImpresionJSON = controladorNumMaxViajes.determinarMaxNumViajes(diasElementos);
-		
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.add("Access-Control-Allow-Origin", "*");
-		
 		ResponseEntity<String> responseEntity =
-				new ResponseEntity<>(listaLineaImpresionJSON, responseHeaders, HttpStatus.CREATED);
-		
+				new ResponseEntity<>(listaLineaImpresionJSON, responseHeaders, HttpStatus.ACCEPTED);
 		return responseEntity;
-		
 	}
 }
