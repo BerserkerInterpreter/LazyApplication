7511c9ba1678c3c2d06b04e2e65d35371a62c4fa
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-08-28 16:00:59 -0500

    New constants
    New constants and class imports.

--- LazyLoadingMicserviceProject/src/main/java/com/lazyloading/constantes/ConstanteMensajes.java
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/constantes/ConstanteMensajes.java
@@ -1,4 +1,4 @@
-package com.lazyloading.constantes;
+package com.lazyloading.constante;
 
 public enum ConstanteMensajes {
 
@@ -11,7 +11,10 @@ public enum ConstanteMensajes {
 	VERIFICAR_LISTA_ELEMENTOS_NULL("com.lazyloading.mensajes.excepcion.verificarListaElementosNull"),
 	VERIFICAR_LISTA_ELEMENTOS_VACIA("com.lazyloading.mensajes.excepcion.verificarListaVacia"),
 	VERIFICAR_ATRAS_LISTA_ELEMENTOS("com.lazyloading.mensajes.excepcion.verificarAtrasListaElementos"),
-	OBTENER_LINEA_IMPRESION("com.lazyloading.mensajes.excepcion.lineaimpresion");
+	OBTENER_LINEA_IMPRESION("com.lazyloading.mensajes.excepcion.lineaimpresion"),
+	LISTA_JSON_NULL("com.lazyloading.mensajes.excepcion.listajsonnull"),
+	CLASE_ELEMENTO_JSON_NULL("com.lazyloading.mensajes.excepcion.claseelementojsonnull"),
+	ADAPTADOR_JSON_NULL("com.lazyloading.mensajes.excepcion.adaptadorjsonnull");
 	
 	private String key;
 	
