6d86d55452602ce36baa84a9a59ab6cd1335fa4e
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-08-28 17:01:20 -0500

    Refactoring and other methods
    Enhance to current class (refactoring and a few methods get added).

--- LazyLoadingMicserviceProject/src/main/java/com/lazyloading/factorias/FactoriaDiasPeriodo.java
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/factorias/FactoriaDiasPeriodo.java
@@ -1,12 +1,12 @@
-package com.lazyloading.factorias;
+package com.lazyloading.factoria;
 
 import com.jcabi.aspects.Loggable;
 import com.lazyloading.configuracion.UtilidadConfiguracionSistema;
-import com.lazyloading.excepciones.ExcepcionDiasPeriodo;
+import com.lazyloading.excepcion.ExcepcionDiasPeriodo;
 import com.lazyloading.modelo.Dia;
 import com.lazyloading.modelo.DiasPeriodo;
 import com.lazyloading.modelo.Elemento;
-import com.lazyloading.utilidades.UtilidadValidadorDiasPeriodo;
+import com.lazyloading.utilidad.UtilidadValidadorDiasPeriodo;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -19,79 +19,83 @@ public class FactoriaDiasPeriodo {
 
 	private static FactoriaDiasPeriodo factoriaDiasPeriodo;
 	
+	private Integer contadorArrayDiasElementos;
+	
 	private FactoriaDiasPeriodo() {}
 	
 	@Loggable(Loggable.INFO)
 	public DiasPeriodo getDiasPeriodo(String diasElementos) throws ExcepcionDiasPeriodo {
-		
 		UtilidadValidadorDiasPeriodo.verificarDiasElementos(diasElementos);
-		
+		String[] arrayDiasElementos = this.obtenerArrayDiasElementos(diasElementos);
+		UtilidadValidadorDiasPeriodo.verificarArrayDiasElementos(arrayDiasElementos);
+		contadorArrayDiasElementos = 0;
+		Integer cantidadDiasPeriodo = this.obtenerCantidadDiasPeriodo(arrayDiasElementos);
+		DiasPeriodo diasPeriodo = new DiasPeriodo();
+		diasPeriodo.setNumeroDias(cantidadDiasPeriodo);
+		Set<Dia> listaDiasEnPeriodo = this.obtenerDiasPeriodo(cantidadDiasPeriodo, arrayDiasElementos);
+		diasPeriodo.setListaDias(listaDiasEnPeriodo);
+		return diasPeriodo;
+	}
+	
+	private String[] obtenerArrayDiasElementos(String diasElementos) {
 		String separador = UtilidadConfiguracionSistema.getSeparador();
 		String reemplazo = UtilidadConfiguracionSistema.getReemplazo();
 		diasElementos = diasElementos.replace(separador, reemplazo);
 		String[] arrayDiasElementos = diasElementos.split(reemplazo);
-		
-		UtilidadValidadorDiasPeriodo.verificarArrayDiasElementos(arrayDiasElementos);
-		
-		int contadorArrDiasElementos = 0;
-		String strVeces = arrayDiasElementos[contadorArrDiasElementos];
-		
-		UtilidadValidadorDiasPeriodo.verificarVeces(strVeces);
-		
-		int veces = UtilidadValidadorDiasPeriodo.obtenerInteger(strVeces);
-		
-		UtilidadValidadorDiasPeriodo.verificarVeces(veces, arrayDiasElementos);
-		
-		contadorArrDiasElementos++;
-
-		DiasPeriodo diasPeriodo = new DiasPeriodo();
-		diasPeriodo.setNumeroDias(veces);
-		
+		return arrayDiasElementos;
+	}
+	
+	private Integer obtenerCantidadDiasPeriodo(String[] arrayDiasElementos) {
+		String strCantidadDiasPeriodo = arrayDiasElementos[contadorArrayDiasElementos];
+		UtilidadValidadorDiasPeriodo.verificarCantidadDiasPeriodo(strCantidadDiasPeriodo);
+		Integer cantidadDiasPeriodo = UtilidadValidadorDiasPeriodo.obtenerInteger(strCantidadDiasPeriodo);
+		UtilidadValidadorDiasPeriodo.verificarCantidadDiasPeriodo(cantidadDiasPeriodo, arrayDiasElementos);
+		contadorArrayDiasElementos++;
+		return cantidadDiasPeriodo;
+	}
+	
+	private Dia obtenerDia(int numeroDia, int cantidadElementos) {
+		Dia dia = new Dia();
+		dia.setNumeroDia(numeroDia);
+		dia.setCantidadElementos(cantidadElementos);
+		return dia;
+	}
+	
+	private List<Elemento> obtenerElementosDia(
+			Integer cantidadElementos,
+			String[] arrayDiasElementos) {
+		List<Elemento> listaElementos = 
+				new ArrayList<>();
+		Integer pesoElemento = 0;
+		for(int j = 0; j < cantidadElementos; j++) {
+			UtilidadValidadorDiasPeriodo.verificarContadorArrDiasElementos(contadorArrayDiasElementos, arrayDiasElementos);
+			String strPesoElemento = arrayDiasElementos[contadorArrayDiasElementos];
+			pesoElemento = UtilidadValidadorDiasPeriodo.obtenerInteger(strPesoElemento);
+			contadorArrayDiasElementos++;
+			Elemento elemento = new Elemento();
+			String unidadPeso = UtilidadConfiguracionSistema.getUnidadPeso();
+			elemento.setPeso(pesoElemento, unidadPeso);
+			listaElementos.add(elemento);
+		}
+		return listaElementos;
+	}
+	
+	private Set<Dia> obtenerDiasPeriodo(
+			Integer cantidadDiasPeriodo, String arrayDiasElementos[]) {
 		Set<Dia> listaDias = new TreeSet<>();
-		
-		int diaFisico = 0;
-		int numeroDia = 1;
-		for(int i = 1; i <= veces; i++) {
-			
-			UtilidadValidadorDiasPeriodo.verificarContadorArrDiasElementos(contadorArrDiasElementos, arrayDiasElementos);
-			
-			String strDiaFisico = arrayDiasElementos[contadorArrDiasElementos];
-			
-			diaFisico = UtilidadValidadorDiasPeriodo.obtenerInteger(strDiaFisico);
-			contadorArrDiasElementos++;
-			
-			Dia dia = new Dia();
-			dia.setNumeroFisico(diaFisico);
-			dia.setNumeroDia(numeroDia);
-			numeroDia++;
-			List<Elemento> listaElementos = 
-					new ArrayList<>();
-			
-			int pesoElemento = 0;
-			for(int j = 0; j < diaFisico; j++) {
-				
-				UtilidadValidadorDiasPeriodo.verificarContadorArrDiasElementos(contadorArrDiasElementos, arrayDiasElementos);
-				
-				String strPesoElemento = arrayDiasElementos[contadorArrDiasElementos];
-				pesoElemento = UtilidadValidadorDiasPeriodo.obtenerInteger(strPesoElemento);
-				contadorArrDiasElementos++;
-				
-				Elemento elemento = new Elemento();
-				String unidadPeso = UtilidadConfiguracionSistema.getUnidadPeso();
-				elemento.setPeso(pesoElemento, unidadPeso);
-				listaElementos.add(elemento);
-				
-			}
-			
+		Integer cantidadElementos = 0;
+		for(Integer numeroDia = 1; numeroDia <= cantidadDiasPeriodo; numeroDia++) {
+			UtilidadValidadorDiasPeriodo.verificarContadorArrDiasElementos(contadorArrayDiasElementos, arrayDiasElementos);
+			String strCantidadElementos = arrayDiasElementos[contadorArrayDiasElementos];
+			cantidadElementos = UtilidadValidadorDiasPeriodo.obtenerInteger(strCantidadElementos);
+			contadorArrayDiasElementos++;
+			Dia dia = this.obtenerDia(numeroDia, cantidadElementos);
+			List<Elemento> listaElementos = this.obtenerElementosDia(cantidadElementos, arrayDiasElementos);
 			Collections.sort(listaElementos);
-			
 			dia.setListaElementos(listaElementos);
 			listaDias.add(dia);
 		}
-		
-		diasPeriodo.setListaDias(listaDias);
-		
-		return diasPeriodo;
+		return listaDias;
 	}
 	
 	public static FactoriaDiasPeriodo getFactoriaDiasPeriodo() {
