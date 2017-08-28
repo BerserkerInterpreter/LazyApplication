0b44dd45e4dff9cbba1b9fe30c6ad47384d11823
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-08-28 16:10:18 -0500

    New class ContadorArrayDiasElementosDTO
    New class for holding a path in iterating of array diasElementos.

---
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/dto/ContadorArrayDiasElementosDTO.java
@@ -0,0 +1,23 @@
+package com.lazyloading.dto;
+
+public class ContadorArrayDiasElementosDTO {
+
+	private Integer contadorArrayDiasElementos;
+	
+	public ContadorArrayDiasElementosDTO() {
+		super();
+	}
+
+	public ContadorArrayDiasElementosDTO(Integer contadorArrayDiasElementos) {
+		this.contadorArrayDiasElementos = contadorArrayDiasElementos;
+	}
+
+	public Integer getContadorArrayDiasElementos() {
+		return contadorArrayDiasElementos;
+	}
+
+	public void setContadorArrayDiasElementos(Integer contadorArrayDiasElementos) {
+		this.contadorArrayDiasElementos = contadorArrayDiasElementos;
+	}
+
+}
