578a50c53aa082ddd3ef35f39f7eea496d432418
J. David Saldarriaga <saldarriagadavid15@gmail.com>
2017-08-28 21:20:09 -0500

    Class upgrades
    New method for adding days and other changes related to renaming.

--- LazyLoadingMicserviceProject/src/main/java/com/lazyloading/modelo/DiasPeriodo.java
+++ LazyLoadingMicserviceProject/src/main/java/com/lazyloading/modelo/DiasPeriodo.java
@@ -4,25 +4,25 @@ import java.util.Set;
 import java.util.TreeSet;
 
 import com.jcabi.aspects.Loggable;
-import com.lazyloading.excepciones.ExcepcionDias;
+import com.lazyloading.excepcion.ExcepcionDias;
 
 public class DiasPeriodo {
 
-	private int numeroDias;
+	private Integer numeroDias;
 	private Set<Dia> listaDias;
 	
 	public DiasPeriodo() {}
 	
-	public DiasPeriodo(int numeroDias, Set<Dia> listaDias) {
+	public DiasPeriodo(Integer numeroDias, Set<Dia> listaDias) {
 		super();
 		this.numeroDias = numeroDias;
 		this.listaDias = listaDias;
 	}
 
-	public int getNumeroDias() {
+	public Integer getNumeroDias() {
 		return numeroDias;
 	}
-	public void setNumeroDias(int numeroDias) {
+	public void setNumeroDias(Integer numeroDias) {
 		this.numeroDias = numeroDias;
 	}
 	public Set<Dia> getListaDias() {
@@ -32,18 +32,23 @@ public class DiasPeriodo {
 		this.listaDias = listaDias;
 	}
 	
+	public void adicionarDia(Dia dia) {
+		Set<Dia> listaDias = this.getListaDias();
+		if(listaDias == null) {
+			listaDias = new TreeSet<>();
+		}
+		listaDias.add(dia);
+	}
+	
 	@Loggable(Loggable.INFO)
 	public Set<LineaImpresion> verificarDias() throws ExcepcionDias {
-		
 		Set<LineaImpresion> listaLineaImpresion 
 				= new TreeSet<>();
-		
 		for(Dia dia : listaDias) {	
 			LineaImpresion lineaImpresion = dia.verificarElementos();
 			listaLineaImpresion.add(lineaImpresion);
 		}
-		
 		return listaLineaImpresion;
-		
 	}
+
 }
